8597b823cafcad3af162628a8caf039e0a2250f8
qydchen <qydchen@gmail.com>
2017-11-17 13:25:02 -0500

    add classes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: "./lib/character.js",
+  entry: "./lib/character/character.js",
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js"
